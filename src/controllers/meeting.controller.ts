831ae3e6ddd43ba97208ffeb8146c9a5249383c7
Shubham Devrani <59960821+shubham1121@users.noreply.github.com>
2026-03-22 19:56:33 +0530

    feat: leave and end meeting apis created

--- src/controllers/meeting.controller.ts
+++ src/controllers/meeting.controller.ts
@@ -4,14 +4,14 @@ import { v4 as uuidv4 } from "uuid";
 
 export const createMeeting = async (req: Request, res: Response) => {
   try {
-    const user = (req as any).user;
+    const userId = req.body.userId;
 
     const meetingId = uuidv4();
 
     const meeting = await Meeting.create({
       meetingId,
-      host: user.userId,
-      participants: [user.userId]
+      host: userId,
+      participants: [userId]
     });
 
     res.status(201).json({
@@ -28,7 +28,7 @@ export const createMeeting = async (req: Request, res: Response) => {
 
 export const joinMeeting = async (req: Request, res: Response) => {
   try {
-    const user = (req as any).user;
+    const userId = req.body.userId;
 
     const meetingId = req.body.meetingId;
 
@@ -46,8 +46,8 @@ export const joinMeeting = async (req: Request, res: Response) => {
     }
     else{
         // Add user to participants if not already in the list
-        if(!meeting.participants.some(id => id.toString() === user.userId)){
-            meeting.participants.push(user.userId);
+        if(!meeting.participants.some(id => id.toString() === userId)){
+            meeting.participants.push(userId);
             await meeting.save();
         }
     }
@@ -61,4 +61,70 @@ export const joinMeeting = async (req: Request, res: Response) => {
       message: "Internal server error"
     });
   }
+};
+
+export const leaveMeeting = async (req: Request, res: Response) => {
+  try{
+    const userId = req.body.userId;
+    const meetingId = req.body.meetingId;
+    const meeting = await Meeting.findOne({ meetingId });
+    if(!meeting){
+      return res.status(404).json({
+        message: "Meeting not found"
+      });
+    }
+    // check if user is part of the meeting
+    if(!meeting.participants.some(id => id.toString() === userId)){
+      return res.status(400).json({
+        message: "User is not part of the meeting"
+      });
+    }
+    // Remove user from participants
+    meeting.participants = meeting.participants.filter(id => id.toString() !== userId);
+    await meeting.save();
+    res.status(200).json({
+      message: "Left the meeting successfully",
+      meeting
+    });
+  }
+  catch (error) {
+  console.error(error);
+  res.status(500).json({
+    message: "Internal server error"
+  });
+}
+};
+
+export const endMeeting = async (req: Request, res: Response) => {
+  try{
+    const userId = req.body.userId;
+    const meetingId = req.body.meetingId;
+    const meeting = await Meeting.findOne({ meetingId });
+    if(!meeting){
+      return res.status(404).json({
+        message: "Meeting not found"
+      });
+    }
+    // check if user is actually host of the meeting
+    if(meeting.host.toString() !== userId){
+      return res.status(403).json({
+        message: "Only the host can end the meeting"
+      });
+    }
+    // End the meeting
+    meeting.isActive = false;
+    //remove all the participants from the meeting
+    meeting.participants = [];
+    await meeting.save();
+    res.status(200).json({
+      message: "Meeting ended successfully",
+      meeting
+    });
+  }
+  catch (error) {
+  console.error(error);
+  res.status(500).json({
+    message: "Internal server error"
+  });
+}
 };
--- src/routes/meeting.routes.ts
+++ src/routes/meeting.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createMeeting, joinMeeting } from "../controllers/meeting.controller";
+import { createMeeting, joinMeeting, leaveMeeting, endMeeting } from "../controllers/meeting.controller";
 import { protect } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.post("/create", protect, createMeeting);
 router.post("/join", protect, joinMeeting);
+router.post("/leave", protect, leaveMeeting);
+router.post("/end", protect, endMeeting);
 
 export default router;
